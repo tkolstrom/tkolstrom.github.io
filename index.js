3890b845153ab626094ccc5cab9bc10b08fb5ab6
Tim Kolstrom <kolstrom@psu.edu>
2017-01-31 18:27:20 -0500

    finish and deploy to S3

---
+++ index.js
@@ -0,0 +1,30 @@
+/**
+ * Created by tkolstrom on 1/31/17.
+ */
+
+$(document).ready(function(){
+
+    $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+
+        $('.navbar-toggle').click();
+
+
+        if (this.has !== "") {
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate( {
+                scrollTop: $(hash).offset().top
+            }, 900, function() {
+                window.location.hash = hash;
+            });
+        }
+    });
+
+
+
+
+});
+
+
